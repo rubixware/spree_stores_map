de37fc5433ac0c958a84d5c45d944850fa7d6294
everblut <emedina@rubixware.com>
2016-06-09 02:36:55 -0500

    change admin namespace

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -1,9 +1,15 @@
-class Admin::StoresController < ApplicationController
-  create.response do |wants|
-   wants.html { redirect_to edit_admin_fancy_thing_url( @fancy_thing ) }
- end
+module Spree
+  module Admin
+    class StoresController < Spree::Admin::BaseController
 
- update.response do |wants|
-   wants.html { redirect_to edit_admin_fancy_thing_url( @fancy_thing ) }
- end
+      create.response do |wants|
+        wants.html { redirect_to edit_admin_fancy_thing_url( @fancy_thing ) }
+      end
+
+       update.response do |wants|
+         wants.html { redirect_to edit_admin_fancy_thing_url( @fancy_thing ) }
+       end
+
+    end
+  end
 end
